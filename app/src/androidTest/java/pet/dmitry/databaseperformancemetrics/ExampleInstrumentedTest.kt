348a53340c24b166af25522c90faf2ec0798be95
Dmitry Borodin <mail@DmitryBorodin.com>
2019-01-09 00:18:20 +0100

    initial

---
+++ app/src/androidTest/java/pet/dmitry/databaseperformancemetrics/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package pet.dmitry.databaseperformancemetrics
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("pet.dmitry.databaseperformancemetrics", appContext.packageName)
+    }
+}
